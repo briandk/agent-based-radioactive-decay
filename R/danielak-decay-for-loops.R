402a6f70531f891aac08cb632aee713be4740a8d
Brian Danielak <briandaniela.k+github@gmail.com>
2017-02-12 15:43:06 -0500

    Fix rbind error
    I keep forgetting that the entries of the new row have to be combined
    via `c()`

--- R/danielak-decay-for-loops.R
+++ R/danielak-decay-for-loops.R
@@ -1,6 +1,5 @@
-library(purrr)
-library(tibble)
 library(magrittr)
+library(tidyverse)
 
 time_steps <- 1:10
 particles <- rep(0, 10000)
@@ -11,7 +10,7 @@ decay_data <- tibble::tribble(
 
 try_to_decay_a_particle <- function(particle) {
   decay_probability <- 0.5
-  does_particle_decay <- (runif(1, 0, 1) >= 0.5)
+  does_particle_decay <- (runif(1, 0, 1) >= decay_probability)
   if (particle == 0 && does_particle_decay) {
     particle <- 1
   }
@@ -21,8 +20,8 @@ try_to_decay_a_particle <- function(particle) {
 for (time_step in time_steps) {
   particles <- map_dbl(particles, try_to_decay_a_particle)
   percent_decayed <- mean(particles) * 100
-  decay_data %>% rbind(
-    time_step, percent_decayed, 100 - percent_decayed
+  decay_data %<>% rbind(
+    c(time_step, percent_decayed, 100 - percent_decayed)
   )
 }
 
